8670501872f6404d530a507780cf7947b622372c
shivanshjais22 <jaisshivansh85@gmail.com>
2025-06-09 12:33:18 +0530

    Update index.js
    to serch

--- SmartContact/src/main/resources/static/js/index.js
+++ SmartContact/src/main/resources/static/js/index.js
@@ -14,4 +14,51 @@ function toggleFunction() {
     $(".sidebar").show(200);
     $(".contain").css("margin-left", "20%");
   }
+  
+  
+};
+
+
+const serch = ()=>{
+	
+	console.log("");
+	
+	let query=$("#serch-input").val();
+
+	
+	if(query==''){
+		$(".serch-result").hide();
+		
+	}else{
+		// sending request to server
+		
+		let url = `http://localhost:6212/serch/${query}`;
+	
+		fetch(url).then((response)=>{
+		return response.json();
+		})
+		.then((data) => {
+			console.log(data);
+			
+			let text = "<div class='list-group'>";
+
+			  data.forEach((contact) => {
+			      text += `<a href='/user/contact/${contact.cid}' class='list-group-item list-group-item-action'>
+			                  ${contact.name}
+			               </a>`;
+			  });
+
+			  text += "</div>";
+
+			
+			  $(".serch-result").html(text);
+			            $(".serch-result").show();
+			
+		});
+		
+		console.log(query);
+		$(".serch-result").show()
+	}
 }
+
+
